2deef19755d0e2473746e38ab243ad71a77b8539
John Gallaugher <john.gallaugher@bc.edu>
2019-03-07 09:18:54 -0500

    Updated comments. Used Pibot name (e.g. pibot.local) instead of IP address.

--- PiBot/ViewController.swift
+++ PiBot/ViewController.swift
@@ -17,8 +17,16 @@ class ViewController: UIViewController {
                                     2: "left",
                                     3: "right"]
     
-    let mqttClient = CocoaMQTT(clientID: "PiBotApp", host: "pibot", port: 1883)
-    // let mqttClient = CocoaMQTT(clientID: "PiBotApp", host: "10.0.1.32", port: 1883)
+    // *** IMPORTANT NOTE: Make sure your host name is entered properly, below ***
+    let mqttClient = CocoaMQTT(clientID: "PiBotApp", host: "pibot.local", port: 1883)
+    // let mqttClient = CocoaMQTT(clientID: "PiBotApp", host: "zerobot.local", port: 1883)
+    // If you still have problems, make sure spelling is exactly as you've named your Pi
+    // and is what you use when you ssh log in.
+    // If that still doesn't work, try removing .local from the name.
+    // And if that still doesn't work, try putting your Pi's IP address in between the quotes
+    // instead of pibot.local.
+    // ** REMEMBER: You'll have to click the "Play" Build/Run button in the upper left corner
+    // each time you modify this code, so that you're running the code with your latest changes.
     
     override func viewDidLoad() {
         super.viewDidLoad()
